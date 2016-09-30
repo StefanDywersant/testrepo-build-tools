58b0ec7e31339be9daec665276904260dc6e93d0
Karol Maciaszek <karol.maciaszek@contractors.roche.com>
2016-09-30 22:16:24 +0200

    Changes

--- cli.js
+++ cli.js
@@ -6,6 +6,8 @@ const exec = require('child_process').exec;
 
 const exec_command = function(cmd) {
 	return new Promise((resolve, reject) => {
+		console.log(cmd);
+
 		exec(cmd, {}, (error, stdout, stderr) => {
 			if (error) {
 				reject(error);
@@ -22,7 +24,17 @@ const exec_command = function(cmd) {
 
 
 const preversion = function() {
-	exec_command('git reset HEAD')
+	return exec_command('git reset HEAD');
+};
+
+
+const postversion = function() {
+	return exec_command('git push');
+};
+
+
+const action = function(func, ...args) {
+	func.apply(undefined, args)
 		.then(
 			() => process.exit(0),
 			(error) => {
@@ -33,9 +45,7 @@ const preversion = function() {
 };
 
 
-const postversion = function() {
-	//exec_command('')
-};
+console.log('invoked with', process.argv.join(' '));
 
 
 if (process.argv.length < 3) {
@@ -44,13 +54,13 @@ if (process.argv.length < 3) {
 }
 
 
-switch(process.argv[2]) {
+switch (process.argv[2]) {
 	case 'preversion':
-		preversion();
+		action(preversion);
 		break;
 
 	case 'postversion':
-		postversion();
+		action(postversion);
 		break;
 
 	default:
