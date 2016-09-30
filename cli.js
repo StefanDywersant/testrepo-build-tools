dfe2d85c43007a67b91547bee562502227ffed76
Karol Maciaszek <karol.maciaszek@contractors.roche.com>
2016-09-30 23:13:24 +0200

    Changes

--- cli.js
+++ cli.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 
-
-const exec = require('child_process').exec,
+const childProcess = require('child_process'),
 	getopt = require('node-getopt'),
 	opn = require('opn'),
 	path = require('path');
 
-
-const exec_command = function(cmd) {
+/**
+ * Executes given command. Prints results to stdout/stderr.
+ * @param {string} cmd Command to be executed
+ * @returns {Promise}
+ */
+const exec = function(cmd) {
 	return new Promise((resolve, reject) => {
-		console.log(cmd);
-
-		exec(cmd, {}, (error, stdout, stderr) => {
+		childProcess.exec(cmd, {}, (error, stdout, stderr) => {
 			if (error) {
 				reject(error);
 				return;
@@ -25,51 +26,35 @@ const exec_command = function(cmd) {
 	});
 };
 
-
-const package_json = function() {
+/**
+ * Reads contents of package.json file.
+ * @returns {object}
+ */
+const packageJson = function() {
 	return require(path.join(process.cwd(), 'package.json'));
 };
 
+/**
+ * Extracts repository URL from package.json.
+ * @returns {string}
+ */
+const repositoryURL = function() {
+	const pkgJson = packageJson();
 
-const repository_url = function() {
-	const pkg_json = package_json();
-
-	if (!pkg_json.hasOwnProperty('repository')) {
+	if (!pkgJson.hasOwnProperty('repository')) {
 		throw new Error('Missing \'repository\' field in package.json');
 	}
 
-	if (pkg_json.repository.isPrototypeOf(String)) {
-		return pkg_json.repository;
+	if (pkgJson.repository.isPrototypeOf(String)) {
+		return pkgJson.repository;
 	}
 
-	if (!pkg_json.repository.hasOwnProperty('url')) {
+	if (!pkgJson.repository.hasOwnProperty('url')) {
 		throw new Error('Missing \'repository.url\' field in package.json');
 	}
 
-	return pkg_json.repository.url;
+	return pkgJson.repository.url;
 };
-
-
-const preversion = function() {
-	return exec_command('git reset HEAD');
-};
-
-
-const postversion = function() {
-	const opt = getopt.create([['v', 'version=ARG']])
-		.parseSystem();
-
-	return exec_command('git push --tags')
-		.then(() => {
-			const changelog_url = repository_url()
-				.replace(/^(git\+https?|git\+ssh):\/\/(.*@)?(.+?)(\.git\/?)?$/, 'https://$3')
-				.concat('/releases/tag/v' + opt.options.version);
-
-			// asynchronous on purpose
-			opn(changelog_url);
-		});
-};
-
 
 const action = function(func, ...args) {
 	func.apply(undefined, args)
@@ -82,20 +67,51 @@ const action = function(func, ...args) {
 		);
 };
 
+// *** ACTIONS ***
 
+/**
+ * NPM's preversion hook
+ * @returns {Promise}
+ */
+const preversion = function() {
+	return exec('git reset HEAD');
+};
+
+/**
+ * NPM's postversion hook
+ * @returns {Promise}
+ */
+const postversion = function(version) {
+	return exec('git push --tags')
+		.then(() => {
+			const changelogURL = repositoryURL()
+				.replace(/^(git\+https?|git\+ssh):\/\/(.*@)?(.+?)(\.git\/?)?$/, 'https://$3')
+				.concat(`/releases/tag/v${version}`);
+
+			// asynchronous on purpose
+			opn(changelogURL);
+		});
+};
+
+// *** MAIN ***
+
+// check arguments
 if (process.argv.length < 3) {
 	console.error('Usage: cli.js <action> [options...]');
 	process.exit(1);
 }
 
-
+// dispatch action
 switch (process.argv[2]) {
 	case 'preversion':
 		action(preversion);
 		break;
 
 	case 'postversion':
-		action(postversion);
+		const opt = getopt.create([['v', 'version=ARG']])
+			.parseSystem();
+
+		action(postversion, opt.options.version);
 		break;
 
 	default:
